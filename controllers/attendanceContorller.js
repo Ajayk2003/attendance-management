ee9104fb6701bf7f1e06f0ca246c634541cd4d1c
Ajay K Simson <110856070+Ajayk2003@users.noreply.github.com>
2023-10-31 21:54:47 +0530

    basic attendance managing has been added

--- controllers/attendanceContorller.js
+++ controllers/attendanceContorller.js
@@ -2,6 +2,14 @@ const { poolPromise } = require("../config/pool");
 const asyncHandler = require("express-async-handler");
 const { getrowbyID, deletebyID } = require("../services/dbFetch");
 
+const getDate = async(date) => {
+  const getDate = `
+  SELECT * from working_days
+  where working_days = STR_TO_DATE( ?, '%Y-%m-%d')`
+  const [result] = await poolPromise.query(getDate, [date])
+  return result[0];
+}
+
 const getAttendance = asyncHandler(async(req, res) => {
   const { degree, year } = req.body;
   getStatement = `
@@ -30,7 +38,7 @@ const addDate = asyncHandler(async(req, res) => {
   res.status(200).json({message : "date added Successfully"});
 });
 
-const deleteDate = async(req, res) => {
+const deleteDate = asyncHandler(async(req, res) => {
   const date = req.body.date;
   if(!date) {
     res.status(400);
@@ -41,11 +49,26 @@ const deleteDate = async(req, res) => {
   where working_days = STR_TO_DATE( ?, '%Y-%m-%d')`
 
   const [result] = await poolPromise.query(deleteStatement, [date]);
+  console.log(result);
   if(!result.affectedRows){
-    res.status(400);
+    res.status(500);
     throw new Error("Error deleting date");
   }
   res.status(200).json({message : `Date deleted ${date} Successfully`});
-}
+});
 
-module.exports = { addDate, deleteDate };
+const addAttendance = asyncHandler(async(req, res) => {
+  const {reg_no, period, date} = req.body;
+  if(!getrowbyID("students","reg_no", reg_no) || !getDate(date)){
+    res.status(404);
+    throw new Error("Student or Date Doesnt exist");
+  }
+
+  const insertStatement = `
+  insert into attendance(reg_no, ${period}) 
+  values(?, 1) `;
+const [result] = await poolPromise.query(insertStatement, [ reg_no])  
+res.status(201).json({message : "attendance added"});
+})
+
+module.exports = { addDate, deleteDate, addAttendance };
--- routes/attendanceRoute.js
+++ routes/attendanceRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { addStaff, updateStaff, deleteStaff } = require('../controllers/staffController');
-const { addDate, deleteDate } = require('../controllers/attendanceContorller');
+const { addDate, deleteDate, addAttendance } = require('../controllers/attendanceContorller');
 
 
 const router = express.Router();
 
+router.route('/').post(addAttendance);
 router.route('/date').post(addDate).delete(deleteDate);
-
 
 module.exports = router;
